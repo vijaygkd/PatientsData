78cf2d9f53dae67d8b4a16ce65f4431efb069a48
Vijay Gaikwad <vig@microsoft.com>
2016-10-12 12:40:29 -0700

    adding attribute routing

--- PatientsData/Controllers/PatientsController.cs
+++ PatientsData/Controllers/PatientsController.cs
@@ -21,6 +21,27 @@ namespace PatientsData.Controllers
         public IEnumerable<Patient> Get()
         {
             return _patients.FindSync(patient => true).ToList();
-        } 
+        }
+
+        public HttpResponseMessage Get(string id)
+        {
+            var patient = _patients.FindSync(p => p.Id == id).First();
+            if (patient == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Patient not found!");
+            }
+            return Request.CreateResponse(patient);
+        }
+
+        [Route("api/patients/{id}/medications")]
+        public HttpResponseMessage GetMedications(string id)
+        {
+            var patient = _patients.FindSync(p => p.Id == id).First();
+            if (patient == null)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Patient not found!");
+            }
+            return Request.CreateResponse(patient.Medications);
+        }
     }
 }
